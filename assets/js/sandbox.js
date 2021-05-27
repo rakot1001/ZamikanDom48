cd48debdd7bfbdee50e804afdfdaee7422cbe75a
Vlad <vladdp70@gmail.com>
2021-05-27 22:45:50 +0300

    Первоначальный вид выполненого задания

--- assets/js/sandbox.js
+++ assets/js/sandbox.js
@@ -5,7 +5,29 @@ function makeCounter() {
     increment() {
       return ++count;
     },
+    decrement() {
+      return --count;
+    },
   };
 }
 
-const counter = makeCounter();
+const counter1 = makeCounter();
+const counter2 = makeCounter();
+
+/*
+Напишите функцию create Adder(n),
+которая вернет другую функцию.Возвращаемая функция должна складывать получаемый аргумент m
+с агргументом n возвращаемой функции.
+const adder(10)=20
+adder(50)=70
+adder(100)=170
+*/
+
+function createAdder(n) {
+  let state = n; //замкнутая переменная инкапсулированая
+  return function (m) {
+    return (n = n + m);
+  };
+}
+
+const adder = createAdder(100);
